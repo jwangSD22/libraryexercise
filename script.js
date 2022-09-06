a270c6fafc56ee14f6da6ec1777ea696fe7286c3
Jaakul <jackw1689@gmail.com>
2022-09-05 18:42:43 -0700

    made functions to create book

--- script.js
+++ script.js
@@ -1,43 +1,79 @@
+const bookContainer = document.getElementById("bookContainer");
 
 let myLibrary = [];
 
 class Book {
-    constructor(title,author,pages,read){
+    constructor(title,author,readStatus,hiddenID){
         this.title = title;
         this.author = author;
-        this.pages = pages;
-        this.readStatus = read;
+        this.readStatus = readStatus;
+        this.hiddenID = hiddenID
     }
+}
+
+function bookMaker(x,y,z){
+    let newBook = new Book(x,y,z)
+    myLibrary.push(newBook);
 }
 
 Book.prototype.textMaker = function (){
-    return `<b>${this.title}</b> written by ${this.author} contains ${this.pages} pages. Status:${this.readStatus}.`
+    return `${this.title} written by ${this.author} contains ${this.pages} pages. Status:${this.readStatus}.`
     }
 
+Book.prototype.deleteBook = function(){
+console.log(this);
+console.log(myLibrary.indexOf(this))
+myLibrary.splice(myLibrary.indexOf(this),1)
+}
 
-const book1 = new Book('Easy Spanish step-by-step',`Barbara Bregstein`,303,'read');
-const book2 = new Book('TED talks','Chris Anderson',269,'not read');
-const book3 = new Book('Japanese soul cooking',"Tadashi Ono",245,'read')
-const book4 = new Book('Harry potter','jk rowling',500,'read');
+Book.prototype.changeStatus = function(){
+    if(this.readStatus === 'Read'){
+        this.readStatus = 'Unread'
+    }
+    else {this.readStatus = 'Read'}
+}
+
+
+
+const book1 = new Book('Easy Spanish step-by-step',`Barbara Bregstein`,'Read',123456);
+const book2 = new Book('TED talks','Chris Anderson','Unread',124678);
+const book3 = new Book('Japanese soul cooking',"Tadashi Ono",'Read',154754)
+const book4 = new Book('Harry potter','jk rowling','Read',125464);
 
 myLibrary.push(book1,book2,book3,book4)
 
-
-function addBooktoLibrary(){
-    for (let i=0;i<myLibrary.length;i++)
-cardMaker.append(myLibrary[i])
-}
 
 
 let z = function(){    
 
     for (let i=0;i<myLibrary.length;i++){
         let y = document.createElement('div');
-        document.body.appendChild(y)
-        let x = document.querySelectorAll('div');
+        y.classList.add('bookInList');
+        bookContainer.appendChild(y)
+        let x = document.querySelectorAll('div.bookInList');
         x[i].textContent=myLibrary[i].textMaker()
     }
 }
+
+
+document.getElementById('submitBook').onclick = function() {
+    var radios = document.getElementsByName('readStatus');
+    let readStatus
+    for (let radio of radios)
+    {
+        if (radio.checked) {
+            readStatus = radio.value;
+        }
+    }
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+
+    bookMaker(title,author,readStatus);
+    document.getElementById('title').value=''
+    document.getElementById('author').value= '';
+    }
+
+
 
 
 
