99b95fa730cadca2f5d484abc376f1ad5fce3cbb
Jaakul <jackw1689@gmail.com>
2022-09-06 11:12:02 -0700

    finished code, need to make everything prettier

---
+++ README.md
@@ -0,0 +1,25 @@
+what did I learn?
+
+-Using object arrays is a great way to store information without losing the power to change the
+information within as well. 
+
+-You have to use a loop to find the information generated from radio buttons
+
+-prototypes are a great way of accessing a function without generating that function on each individual object
+
+-x- must look into prototypal inheritance. Not sure if generating prototypes in the correct way.
+
+-gained more experience in navigating the dom structure
+
+-gained more experience using "this".
+
+ I believe using "this" has a big relation towards using prototype inherited functions
+
+-fun second experience using a recursive function to remove children from a parent node.(( while a child exists, keep removing the child))
+
+**is there a better way to adjust this list without having to delete the whole list and repopulating it every single time something is changed within the object array? Can this be done dynamically?? maybe it's something another tool needs to manage.
+
+**need to fix CSS to make this prettier
+
+**need to add code to consider that the index# generator could potentially generate a duplicate number, which would result in errors. It should checked if the index# already exists as a property in the array.
+
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bookContainer = document.getElementById("bookContainer");
+const buttonGrabber = document.getElementsByClassName('buttonDiv')
 
 let myLibrary = [];
 
@@ -11,14 +12,11 @@ class Book {
     }
 }
 
-function bookMaker(x,y,z){
-    let newBook = new Book(x,y,z)
+function bookMaker(x,y,z,i){
+    let newBook = new Book(x,y,z,i)
     myLibrary.push(newBook);
+    return newBook
 }
-
-Book.prototype.textMaker = function (){
-    return `${this.title} written by ${this.author} contains ${this.pages} pages. Status:${this.readStatus}.`
-    }
 
 Book.prototype.deleteBook = function(){
 console.log(this);
@@ -35,13 +33,14 @@ Book.prototype.changeStatus = function(){
 
 
 
-const book1 = new Book('Easy Spanish step-by-step',`Barbara Bregstein`,'Read',123456);
-const book2 = new Book('TED talks','Chris Anderson','Unread',124678);
-const book3 = new Book('Japanese soul cooking',"Tadashi Ono",'Read',154754)
-const book4 = new Book('Harry potter','jk rowling','Read',125464);
+
+const book1 = new Book('Easy Spanish step-by-step',`Barbara Bregstein`,'Read','123456');
+const book2 = new Book('TED talks','Chris Anderson','Unread','124678');
+const book3 = new Book('Japanese soul cooking',"Tadashi Ono",'Read','154754')
+const book4 = new Book('Harry potter','jk rowling','Read','125464');
 
 myLibrary.push(book1,book2,book3,book4)
-
+pushBook();
 
 
 let z = function(){    
@@ -65,16 +64,85 @@ document.getElementById('submitBook').onclick = function() {
             readStatus = radio.value;
         }
     }
+    //random 6 digit Index number generator
+    let num = Math.random().toString();
+    let newIndex = num.slice(2,8);
+
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
-
-    bookMaker(title,author,readStatus);
+    bookMaker(title,author,readStatus,newIndex);
     document.getElementById('title').value=''
     document.getElementById('author').value= '';
+    pushBook()
     }
 
 
+//function that pushes the whole array to list
+function pushBook(){
+while(bookContainer.firstChild){
+    bookContainer.removeChild(bookContainer.firstChild);
+}
+
+for(let i=0;i<myLibrary.length;i++){
+
+var x = document.createElement("BUTTON");
+var t = document.createTextNode("✓");
+var no = document.createTextNode("⨯");
+x.setAttribute('hiddenID',myLibrary[i].hiddenID);
+x.setAttribute('hiddenID',myLibrary[i].hiddenID);
+x.setAttribute('id','readStatusButton');
+
+if(myLibrary[i].readStatus === 'Read'){
+    x.appendChild(t)
+}
+else{x.appendChild(no)}
+
+
+var y = document.createElement("BUTTON");
+var z = document.createTextNode("DELETE");
+y.setAttribute('hiddenID',myLibrary[i].hiddenID);
+y.setAttribute('id','listDeleteButton');
+y.appendChild(z);
+
+let divMaker1 = document.createElement('div')
+let divMaker2 = document.createElement('div')
+let divMaker3 = document.createElement('div')
+let divMaker4 = document.createElement('div')
+
+divMaker1.setAttribute('hiddenID',myLibrary[i].hiddenID);
+divMaker2.setAttribute('hiddenID',myLibrary[i].hiddenID);
+divMaker3.setAttribute('hiddenID',myLibrary[i].hiddenID);
+divMaker3.setAttribute('class','buttonDiv');
+divMaker3.setAttribute('onclick','buttonStatusChanger(this)');
+divMaker3.setAttribute('hiddenID',myLibrary[i].hiddenID);
+divMaker4.setAttribute('hiddenID',myLibrary[i].hiddenID);
+divMaker4.setAttribute('onclick','buttonDeleteBook(this)');
+
+divMaker1.textContent = myLibrary[i].title
+divMaker2.textContent = myLibrary[i].author
+divMaker3.textContent = myLibrary[i].readStatus + '   '
+divMaker3.appendChild(x);
+divMaker4.appendChild(y);
+
+bookContainer.appendChild(divMaker1)
+bookContainer.appendChild(divMaker2)
+bookContainer.appendChild(divMaker3)
+bookContainer.appendChild(divMaker4)
+}
+}
 
 
 
+function buttonStatusChanger(x){
+    let id = x.getAttribute('hiddenID')
+    let index = myLibrary.findIndex(item=>item.hiddenID===id);
+    myLibrary[index].changeStatus();
+    pushBook();
+    }
 
+function buttonDeleteBook(x){
+    let id = x.getAttribute('hiddenID');
+    let index = myLibrary.findIndex(item=>item.hiddenID===id);
+    myLibrary[index].deleteBook();
+    pushBook();
+    }
